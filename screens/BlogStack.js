1db0ea2641d240ecf10d4daf9bdede4d3acd5e9a
Sebastian Choo <sebastianchoo@nutella.local>
2021-03-24 11:58:59 +0800

    first commit

---
+++ screens/BlogStack.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import IndexScreen from "./IndexScreen";
+import AddScreen from "./AddScreen";
+import EditScreen from './EditScreen';
+
+const InnerStack = createStackNavigator();
+
+export default function BlogStack() {
+  return (
+    <InnerStack.Navigator mode="modal">
+      <InnerStack.Screen
+        name="Blogs"
+        component={IndexScreen}
+        options={{
+          title: "Blogs",
+          headerStyle: {
+            backgroundColor: "yellow",
+            height: 100,
+            shadowColor: "black",
+            shadowOpacity: 0.2,
+            shadowRadius: 5,
+          },
+          headerTintColor: "#f55",
+          headerTitleStyle: {
+            fontSize: 24,
+            fontWeight: "bold",
+          },
+        }}
+      />
+      <InnerStack.Screen name="Add" component={AddScreen}/>
+      <InnerStack.Screen name="Edit" component={EditScreen}/>
+
+    </InnerStack.Navigator>
+  );
+}
---
+++ screens/IndexScreen.js
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity, FlatList } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+
+export default function IndexScreen({ navigation, route }) {
+  const [blogs, setBlogs] = useState([]);
+
+  // This is to set up the top right button
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity onPress={addPost}>
+          <FontAwesome
+            name="edit"
+            size={30}
+            color="black"
+            style={{
+              color: "#f55",
+              marginRight: 10,
+            }}
+          />
+        </TouchableOpacity>
+      ),
+    });
+  });
+
+  // Monitor route.params for changes and add items to the database
+
+  function addPost() {
+    navigation.navigate("Add Screen");
+  }
+
+  // This deletes an individual note
+  function deleteNote(id) {
+    console.log("Deleting " + id);
+    
+  }
+
+  // The function to render each row in our FlatList
+  function renderItem({ item }) {
+    return (
+      <View
+        style={{
+          padding: 10,
+          paddingTop: 20,
+          paddingBottom: 20,
+          borderBottomColor: "#ccc",
+          borderBottomWidth: 1,
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        <Text>{item.title}</Text>
+        <TouchableOpacity onPress={() => deleteNote(item.id)}>
+          <Ionicons name="trash" size={16} color="#944" />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={blogs}
+        renderItem={renderItem}
+        style={{ width: "100%" }}
+        keyExtractor={(item) => item.id.toString()}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ffc",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
